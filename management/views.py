33af742f4f746d8c657e1eb04dc9d75af414b94f
ali <ali.darzi.1354@gmail.com>
2024-07-01 13:36:59 +0330

    project Post|Put|Get|List|Patch|Delete provided !

--- management/views.py
+++ management/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from management.models import Project
+from management.serializers import ProjectSerializer
+
 
 # Create your views here.
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    serializer_class = ProjectSerializer
+    queryset = Project.objects.all()
