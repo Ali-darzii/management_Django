9348ec1cee62e47cca5868da52b55d5cce2cd0ca
ali <ali.darzi.1354@gmail.com>
2024-07-01 13:15:33 +0330

    managment tables provided !

--- management/models.py
+++ management/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=225)
+    description = models.TextField(null=True, default=None)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} | {self.created_at.year}"
+
+    class Meta:
+        verbose_name = "Project"
+        verbose_name_plural = 'Projects'
+        db_table = 'projectTable'
+
+
+class Task(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
+    title = models.CharField(max_length=225)
+    description = models.TextField(null=True, default=None)
+
+    status_choices = [('pending', 'Pending'), ('in_progress', 'In Progress'), ('completed', 'Completed')]
+    status = models.CharField(choices=status_choices, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    due_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.project.name} | {self.title}"
+
+    class Meta:
+        verbose_name = "Task"
+        verbose_name_plural = 'Tasks'
+        db_table = 'taskTable'
+
+
+class Comment(models.Model):
+    task = models.ForeignKey(Task, related_name='comments', on_delete=models.CASCADE)
+    author = models.CharField(max_length=225)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.task.project.name} | {self.task.title} | {self.author}"
+
+    class Meta:
+        verbose_name = "Comment"
+        verbose_name_plural = 'Comments'
+        db_table = 'commentTable'
